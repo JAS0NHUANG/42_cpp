9cf8c6421b688974a67523a3e91b2f5d1b599a09
Jason HUANG <jahuang@e2r5p3.clusters.42paris.fr>
2022-03-11 15:38:55 +0100

    school linux

---
+++ module00/ex01/srcs/megaphone.cpp
@@ -0,0 +1,20 @@
+#include "megaphone.hpp"
+
+int	main(int ac, char **av)
+{
+	std::string	str_holder;
+
+	if (ac < 2)
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *\n";	
+	else
+	{
+		for (int i = 1; i < ac; i++)
+		{
+			str_holder = av[i];
+			for (int j = 0; j < str_holder.length(); j++)
+				std::cout << (char)toupper(str_holder[j]);
+		}
+		std::cout << '\n';
+	}	
+	return (0);
+}
