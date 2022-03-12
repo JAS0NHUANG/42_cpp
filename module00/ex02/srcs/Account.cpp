7d5fae74a8eb047fbf44be8a641e8d70c298865b
Jason HUANG <jahuang@e3r3p7.clusters.42paris.fr>
2022-03-12 17:38:39 +0100

    module00 seems ok

--- module00/ex02/srcs/Account.cpp
+++ module00/ex02/srcs/Account.cpp
@@ -36,31 +36,31 @@ Account::~Account( void )
 
 int	Account::getNbAccounts( void )
 {
-	return (_nbAccounts);
+	return (Account::_nbAccounts);
 }
 
 int	Account::getTotalAmount( void )
 {
-	return (_totalAmount);
+	return (Account::_totalAmount);
 }
 
 int	Account::getNbDeposits( void )
 {
-	return (_totalNbDeposits);
+	return (Account::_totalNbDeposits);
 }
 
 int	Account::getNbWithdrawals( void )
 {
-	return (_totalNbWithdrawals);
+	return (Account::_totalNbWithdrawals);
 }
 
 void	Account::displayAccountsInfos( void )
 {
 	Account::_displayTimestamp();
-	cout << "accounts:" << _nbAccounts << ";" \
-		<< "total:" << _totalAmount << ";" \
-		<< "deposits:" << _totalNbDeposits << ";" \
-		<< "withdrawals:" << _totalNbWithdrawals << "\n";
+	cout << "accounts:" << Account::getNbAccounts() << ";" \
+		<< "total:" << Account::getTotalAmount() << ";" \
+		<< "deposits:" << Account::getNbDeposits() << ";" \
+		<< "withdrawals:" << Account::getNbWithdrawals() << "\n";
 	return ;
 }
 
