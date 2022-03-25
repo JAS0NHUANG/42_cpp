029ed754d37bff0e7064395e5c979c8658ac35e5
Jason HUANG <jahuang@e1r11p1.42.fr>
2022-03-25 14:48:39 +0100

    fix module00 ex00

---
+++ module00/ex00/srcs/megaphone.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+
+int	main(int ac, char **av) {
+	std::string	str_holder;
+
+	if (ac < 2) {
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *\n";
+		return (0);
+	}
+	for (int i = 1; i < ac; i++)
+	{
+		str_holder = av[i];
+		for (int j = 0; j < (int)str_holder.length(); j++)
+			std::cout << (char)toupper(str_holder[j]);
+	}
+	std::cout << '\n';
+	return (0);
+}
