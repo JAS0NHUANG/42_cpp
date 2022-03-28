fdbbd14b6a46f69bcd92905d7a7e693b3cfc13e1
Jason HUANG <jahuang@e2r7p7.clusters.42paris.fr>
2022-03-28 12:26:55 +0200

    module01 start

---
+++ module01/ex00/incs/Zombie.hpp
@@ -0,0 +1,14 @@
+#ifndef __ZOMBIE_HPP__
+# define __ZOMBIE_HPP__
+
+# include <iostream>
+# include <string>
+
+class	Zombie {
+	public:
+		Zombie( std::string name );
+		~Zombie( void );
+	private:
+}
+
+#endif
