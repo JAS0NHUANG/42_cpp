75a6abc7351f436c6b19f0759f32c6a7aa5ee745
jas0nhuang <jas0nhuang>
2022-04-18 13:10:23 +0200

    module01 ex04 ok

--- module01/ex04/main.cpp
+++ module01/ex04/main.cpp
@@ -2,48 +2,51 @@
 #include <iostream>
 #include <string>
 
+int	return_err(int err_code, char *filename)
+{
+	if (err_code == 1)
+		std::cerr << "Wrong amount of arguments.\n";
+	if (err_code == 2)
+		std::cerr << "Error opening file: " << filename << "\n";
+	return (err_code);
+}
+
+int	replace_string_in_file(std::string to_find, std::string to_replace,
+		std::fstream &the_file, std::string filename)
+{
+	std::ofstream replace_file(filename.append(".replace").c_str());
+	std::string str_buff;
+	std::size_t found_i = 0;
+	std::size_t index = 0;
+	while (getline(the_file, str_buff)) {
+		index = 0;
+		found_i = str_buff.find(to_find);
+		while (found_i != std::string::npos)
+		{
+			replace_file << str_buff.substr(index, found_i - index);
+			replace_file << to_replace;
+			index = found_i + to_find.length();
+			found_i = str_buff.find(to_find, index);
+		}
+		if (index < str_buff.length())
+			replace_file << str_buff.substr(index, str_buff.length() - index);
+		replace_file << "\n";
+	}
+	return (0);
+}
+
 int	main(int ac, char **av) {
 	if (ac != 4)
-	{
-		std::cerr << "Wrong amount of arguments.\n";
-		return (1);
-	}
+		return (return_err(1, NULL));
 	std::string filename = av[1];
-	std::ifstream the_file(av[1]);
-	std::ofstream replace_file(filename.append(".replace").c_str());
+	std::fstream the_file;
 
+	the_file.open(av[1], std::ios_base::in);
 	if (the_file.is_open()) {
-		std::string the_string;
-		std::string str_to_find = av[2];
-		std::string str_to_replace = av[3];
-		std::size_t found_i = 0;
-		std::size_t index = 0;
-		std::cout << "init found: " << found_i << "\n";
-
-		while (getline(the_file, the_string)) {
-			index = 0;
-			found_i = the_string.find(str_to_find);
-			std::cout << the_string << "\n";
-			while (found_i != std::string::npos)
-			{
-				std::cout << "found: " << found_i << "\n";
-				/* can't use replace.... :'(
-				the_string.replace(found_i, 1, "kkkkk");\
-				*/
-				std::cout << "index to found_i: " << index << " / " << found_i << "\n";
-				replace_file << the_string.substr(index, found_i - index);
-				replace_file << str_to_replace;
-				index = found_i + str_to_find.length();
-				std::cout << "index: " << index << "\n";
-				found_i = the_string.find(str_to_find, index);
-			}
-			if (index < the_string.length())
-				replace_file << the_string.substr(index, the_string.length() - index);
-			replace_file << "\n";
-		}
-
-	} else {
-		std::cout << "Error opening file: " << av[1] << "\n";
-	}
+		std::string to_find = av[2];
+		std::string to_replace = av[3];
+		replace_string_in_file(to_find, to_replace, the_file, filename);
+	} else
+		return (return_err(1, av[1]));
 	return (0);
 }
