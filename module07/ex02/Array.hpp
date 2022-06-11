f335ae2b76137cb14a2e4742974547551e1f8ac5
jas0nhuang <jas0nhuang>
2022-06-11 23:46:19 +0200

    on macbook air

--- module07/ex02/Array.hpp
+++ module07/ex02/Array.hpp
@@ -1,15 +1,27 @@
+#include <iostream>
+
 template<typename T>
 class Array {
 	public:
-		Array(void);
-		~Array(void);
+		// canonical
+		Array(void) : _array(new T[0]), _size(0) {}
+		~Array(void) {}
+		Array(Array const & toCopy) { *this = toCopy; }
+		Array &operator=(Array const & toAssign) {
+			this->_array = toAssign._array;
+			return (*this);
+		}
+		Array(unsigned int n) : _array(new T[n]), _size(n) {}
 
-		Array(unsigned int n);
-		Array(Array & toCopy);
-
-		unsigned int size(void) const;
+		// member functions
+		T & operator[](unsigned int index) { 
+			if (index < 0 || index >= this->size())
+				throw std::exception();
+			return (this->_array[index]);
+		}
+		unsigned int size(void) const { return (this->_size); }
 
 	private:
 		T				*_array;
 		unsigned int	_size;
-}
+};
