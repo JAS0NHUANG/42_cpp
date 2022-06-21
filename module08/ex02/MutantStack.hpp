e6ef12be6308ce4662ca799fc7518e074d3b8435
jas0nhuang <jas0nhuang>
2022-06-21 03:23:28 +0200

    OK

--- module08/ex02/MutantStack.hpp
+++ module08/ex02/MutantStack.hpp
@@ -6,11 +6,22 @@
 
 template <typename T>
 class	MutantStack : public std::stack<T> {
-
 	public:
+		// canonical
+		MutantStack(void) {};
+		~MutantStack(void) {};
+		MutantStack(const MutantStack &toCopy);
+		MutantStack	&operator=(const MutantStack &toAssign);
+
+		// typedef
 		typedef typename std::stack<T>::container_type::iterator iterator;
+		typedef typename std::stack<T>::container_type::const_iterator const_iterator;
+
+		// iterators
 		iterator	begin(void) {return (this->c.begin()); }
 		iterator	end(void) {return (this->c.end()); }
+		const_iterator	begin(void) const {return (this->c.begin()); }
+		const_iterator	end(void) const {return (this->c.end()); }
 };
 
 #endif // MUTANTSTACK_HPP_
--- module08/ex02/main.cpp
+++ module08/ex02/main.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <string>
+#include <vector>
 
 #include "MutantStack.hpp"
 
@@ -23,8 +25,70 @@ int	main() {
 	std::cout << "All the stack methods: \n";
 	std::cout << "top: " << mStack.top() << "\n";
 
+	std::cout << "Normal iterator tests: \n";
 	it = mStack.begin();
 	ite = mStack.end();
 	std::cout << "Value inside the begin iterator: " <<  *(it) << "\n";
 	std::cout << "Value inside the end - 1 iterator: " << *(ite - 1) << "\n";
+
+	while (it != ite) {
+		int	value = *(it);
+		std::cout << value << "\n";
+		*(it) = value + 1;
+		it++;
+	}
+	it = mStack.begin();
+	std::cout << "Value inside the begin iterator: " <<  *(it) << "\n";
+	std::cout << "Value inside the end - 1 iterator: " << *(ite - 1) << "\n";
+
+	std::cout << "\n";
+	std::cout << "Const iterator tests: \n";
+	MutantStack<int>::const_iterator	const_it;	
+	MutantStack<int>::const_iterator	const_ite;	
+
+	const_it = mStack.begin();
+	const_ite = mStack.end();
+	std::cout << "Value inside the begin const iterator: " <<  *(const_it) << "\n";
+	std::cout << "Value inside the end - 1 const iterator: " << *(const_ite - 1) << "\n";
+	/* Can not modify values by const_iterator. This part won't compile.
+	while (const_it != const_ite) {
+		int	value = *(const_it);
+		std::cout << value << "\n";
+		*(const_it) = value + 1;
+		const_it++;
+	}
+	it = mStack.begin();
+	std::cout << "Value inside the begin iterator: " <<  *(it) << "\n";
+	std::cout << "Value inside the end - 1 iterator: " << *(ite - 1) << "\n";
+	*/
+
+	MutantStack<std::string> mStrStack;
+	MutantStack<std::string>::iterator	strIt;	
+	MutantStack<std::string>::iterator	strIte;	
+
+	std::cout << "Push string hello into the mutant stack.\n";
+	mStrStack.push("hello");
+
+	std::cout << "Push string world into the mutant stack.\n";
+	mStrStack.push("world");
+
+	std::cout << "Push string 42 into the mutant stack.\n";
+	mStrStack.push("42");
+	std::cout << "Mutant stack size: " << mStrStack.size() << "\n";
+
+	std::cout << "All the stack methods: \n";
+	std::cout << "top: " << mStrStack.top() << "\n";
+
+	std::cout << "Normal iterator tests: \n";
+	strIt = mStrStack.begin();
+	strIte = mStrStack.end();
+	std::cout << "Value inside the begin iterator: " <<  *(strIt) << "\n";
+	std::cout << "Value inside the end - 1 iterator: " << *(strIte - 1) << "\n";
+ 
+
+	std::vector<int> intV;
+
+	std::vector<int>::iterator vecIt;
+	vecIt = intV.begin();
+	std::cout << *vecIt;
 }
