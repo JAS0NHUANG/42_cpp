d43e895e54f2a8c4b3f5dde371f021620570a178
jahuang <jahuang>
2023-04-24 22:59:18 +0900

    nearly~~~~~~

--- module09/ex02/merge_insertion_sort.cpp
+++ module09/ex02/merge_insertion_sort.cpp
@@ -9,7 +9,8 @@
 /*******************************************************************************/
 template<typename T>
 void	print_int_array(T& int_array) {
-	std::cout << "The int array: ";
+	std::cout << "array length: " << int_array.size();
+	std::cout << "\narray content: ";
 	for (int i = 0; i < int_array.size(); i++) {
 		std::cout << int_array[i];
 		if (i != (int_array.size() - 1) )
@@ -36,8 +37,6 @@ void	swap_n_element(T& int_array, int n, int index) {
 }
 
 void	last_insert(std::deque<int>& main_chain) {
-	std::cout << "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n";
-	std::cout << "!!! in last_insert() function !!!\n";
 	std::deque<int> main_chain_holder;
 	std::deque<int> sub_chain;
 	int	rest = -1;
@@ -53,8 +52,6 @@ void	last_insert(std::deque<int>& main_chain) {
 		main_chain_holder.push_back(main_chain[i]);
 		i++;
 	}
-	std::cout << "main_holder: ";
-	print_int_array(main_chain_holder);
 	main_chain.clear();
 	std::move(begin(main_chain_holder), end(main_chain_holder), back_inserter(main_chain));
 
@@ -68,7 +65,6 @@ void	last_insert(std::deque<int>& main_chain) {
 			}
 		}
 	}
-	std::cout << "rest here: " << rest;
 	if (rest != -1) {
 		for (int j = main_chain.size() - 1; j >=0; j--) {
 			if (rest > main_chain[j]) {
@@ -79,7 +75,6 @@ void	last_insert(std::deque<int>& main_chain) {
 			}
 		}
 	}
-	std::cout << "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n";
 }
 
 template<typename T>
@@ -87,12 +82,6 @@ void	insert(T& int_array, int pair_by) {
 	int rest = int_array.size() % pair_by;
 	int	pairs = int_array.size() / pair_by;
 	std::deque<int> holder_array;
-
-	std::cout << "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n";
-	std::cout << "!!! in insert() function !!!\n";
-	std::cout << "pair_by: " << pair_by << "\n";
-	std::cout << "rest: " << rest << "\n";
-	std::cout << "pairs: " << pairs << "\n";
 
 	if (pairs == 1 && rest != pair_by)
 		return;
@@ -116,14 +105,9 @@ void	insert(T& int_array, int pair_by) {
 		}
 	}
 
-	print_int_array(main_chain);
-	print_int_array(int_array);
-
 	for (int i = pair_by; i < int_array.size() - rest; i += (pair_by * 2)) {
 		// find the position to insert from the end of the main_chain
 		for (int j = main_chain.size() - pair_by; j >= 0; j -= pair_by) {
-			std::cout << "the int_array i: "<< int_array[i] <<"\n";
-			std::cout << "the main_chain j: "<< main_chain[j] <<"\n";
 			if (int_array[i] > main_chain[j]) {
 				for (int k = 0; k < pair_by; k++) {
 					main_chain.insert(main_chain.begin() + j + pair_by + k, int_array[i + k]);
@@ -132,14 +116,10 @@ void	insert(T& int_array, int pair_by) {
 			}
 		}
 	}
-	print_int_array(main_chain);
 	int_array.clear();
 	std::move(begin(main_chain), end(main_chain), back_inserter(int_array));
 
 	// check if we need to insert the rest
-	std::cout << "!!! check if we need to insert the ~~rest~~ !!!\n";
-	print_int_array(main_chain);
-	print_int_array(holder_array);
 	if (pair_by == 2 && holder_array.size() == 1) {
 		// do the last insert
 		if (holder_array.size() != 0) {
@@ -147,37 +127,27 @@ void	insert(T& int_array, int pair_by) {
 				main_chain.push_back(holder_array[i]);
 			}
 		}
-		std::cout << "before last insert: ";
-		print_int_array(main_chain);
-
-		last_insert(main_chain);
 	} else if (rest != pair_by / 2 && pair_by != 2){
-		std::cout << "no rest ins!!!!!\n";
-
 		// no insert of the rest, just add back to int_array
 		std::move(begin(holder_array), end(holder_array), back_inserter(main_chain));
-		print_int_array(main_chain);
 	} else {
-		std::cout << "yes rest ins!!!!!\n";
-		for (int i = 0; i < holder_array.size(); i++) {
 		// find the position to insert from the end of the main_chain
-			for (int j = main_chain.size() - pair_by; j >= 0; j -= pair_by) {
-				if (holder_array[i] > main_chain[j]) {
-					for (int k = 0; k < pair_by; k++) {
-						main_chain.insert(main_chain.begin() + j + pair_by + k, holder_array[i + k]);
-					}
-				} else if (j == 0) {
-					for (int k = 0; k < pair_by; k++) {
-						main_chain.insert(main_chain.begin() + j + k, holder_array[i + k]);
-					}
+		for (int j = main_chain.size() - pair_by; j >= 0; j -= pair_by) {
+			if (j > 0 && holder_array[0] > main_chain[j]) {
+				for (int k = 0; k < pair_by; k++) {
+					main_chain.insert(main_chain.begin() + j + pair_by + k, holder_array[k]);
+				}
+				break ;
+			} else if (j == 0) {
+				for (int k = 0; k < pair_by; k++) {
+					main_chain.insert(main_chain.begin() + j + k, holder_array[k]);
 				}
 			}
 		}
 	}
 	int_array.clear();
+	last_insert(main_chain);
 	std::move(begin(main_chain), end(main_chain), back_inserter(int_array));
-	std::cout << "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n";
-
 }
 
 template<typename T>
@@ -216,14 +186,21 @@ int main(int ac, char **av) {
 
 	// sort deques
 	std::deque<int> deque_a = {7, 1, 3, 5, 22, 13};
-	std::deque<int> deque_b = {98,  76, 199, 2, 3 ,44, 188 , 99, 5, 38};
+	std::deque<int> deque_b = {98,  76, 199, 2, 3 ,44, 188 , 99, 5, 38, 1, 4, 7, 6, 9, 8, 10, 14, 27, 110};
+	std::deque<int> deque_c = {98,  76, 199, 2, 3 ,44, 188 , 99, 5};
 
 	std::cout << "\n\nbefore sort: \n";
+	print_int_array(deque_a);
 	print_int_array(deque_b);
+	print_int_array(deque_c);
 
 	std::cout << "\n\nsort deques: \n";
+	merge_insertion_sort(deque_a, 1);
 	merge_insertion_sort(deque_b, 1);
+	merge_insertion_sort(deque_c, 1);
 
 	std::cout << "After sort: ";
+	print_int_array(deque_a);
 	print_int_array(deque_b);
+	print_int_array(deque_c);
 }
